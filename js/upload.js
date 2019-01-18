dbb399519c36d7e6db8e812eec3e3df44c4049b2
Anastasia Kalinina <stasya.kalinina@gmail.com>
2019-01-18 18:48:11 +0500

    функция загрузки изображений

--- js/upload.js
+++ js/upload.js
@@ -1,19 +1,39 @@
 'use strict';
 
 (function () {
-  var imagesChooser = document.querySelector('.ad-form__upload');
+//  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var MAX_PHOTO = 11;
+  var imagesChooser = document.querySelector('#images');
   var previewContainer = document.querySelector('.ad-form__photo-container');
+  var previewPhotoBlock = previewContainer.querySelector('.ad-form__photo');
 
-  var addPhotos = function () {
+  var uploadPhotos = function () {
     for (var i = 0; i < imagesChooser.files.length; i++) {
       var file = imagesChooser.files[i];
       var reader = new FileReader();
-      reader.onload = function (e) {
-        previewContainer.appendChild();
-      };
+
+      reader.addEventListener('load', function () {
+        if (!previewPhotoBlock.children.length) {
+          var housePhoto = document.createElement('img');
+          housePhoto.width = 70;
+          housePhoto.height = 70;
+          housePhoto.src = reader.result;
+          previewPhotoBlock.appendChild(housePhoto);
+
+        } else if (previewContainer.children.length < MAX_PHOTO) {
+          var photoContainer = document.createElement('div');
+          photoContainer.classList.add('ad-form__photo');
+          housePhoto = document.createElement('img');
+          housePhoto.width = 70;
+          housePhoto.height = 70;
+          housePhoto.src = reader.result;
+          previewContainer.appendChild(photoContainer);
+          photoContainer.appendChild(housePhoto);
+        }
+      });
       reader.readAsDataURL(file);
     }
   };
 
-  imagesChooser.addEventListener('change', addPhotos);
+  imagesChooser.addEventListener('change', uploadPhotos);
 })();
